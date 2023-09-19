1ab64a008d16fd2ffa0ab93443f2f5fc160f1f07
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-09-19 13:28:04 -0400

    typescript

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Liya Getachew
+ * Created on: Sep 2023
+ * This program calculates area and perimeter.
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString("A rectangle has dimensions 5cm & 3cm.")
+basic.showString("The perimeter equals " + (2 * (5 + 3)).toString() + "cm")
+basic.showString('The area equals ' + (5 * 3).toString() + "cm^2")
