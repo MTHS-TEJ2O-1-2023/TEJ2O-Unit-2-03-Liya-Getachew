a52c7553c7481fac95a3ba4167ce371aa0dbc554
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-09-20 17:11:26 +0000

    python vers

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,15 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Liya Getachew
+Created on: Sep 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.clear()
+sleep(1)
+
+display.scroll("A rectangle has dimensions 5cm & 3cm.'")
+display.scroll("The perimeter equals " + str(2 * (5 + 3)) + "cm^2")
+display.scroll("The area equals " + str(5 * 3) + "cm^2")
